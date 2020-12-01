e3d3c57b5ed4a1edcb88fd161b980b734470b8da
evel0219 <evgenii.elizarov@netcracker.com>
2020-12-01 11:23:30 +0300

    Lab2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import jade.Boot;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] services = {"-giu", "-agents", "agentSellerFromOnline:SellerFromOnline"};
+        Boot.main(services);
+        String[] services2 = {"-giu", "-agents", "agentSellerFromShop:SellerFromShop"};
+        Boot.main(services2);
+        String[] services3 = {"-giu", "-agents", "agentBuyer:Buyer"};
+        Boot.main(services3);
+    }
+}
---
+++ src/SellerFromShop.java
@@ -0,0 +1,99 @@
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.Hashtable;
+
+public class SellerFromShop extends Agent {
+    private Hashtable catalogue;
+
+    protected void setup() {
+        catalogue = new Hashtable();
+
+        DFAgentDescription dfAgentDescription = new DFAgentDescription();
+        dfAgentDescription.setName(getAID());
+        ServiceDescription serviceDescription = new ServiceDescription();
+        serviceDescription.setType("book-selling");
+        serviceDescription.setName("Seller From Shop");
+        dfAgentDescription.addServices(serviceDescription);
+        try {
+            DFService.register(this, dfAgentDescription);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        addBehaviour(new SellerFromShop.OfferRequestsServer());
+
+        addBehaviour(new SellerFromShop.PurchaseOrdersServer());
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+    }
+
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                catalogue.put(title, new Integer(price));
+                System.out.println(title+" inserted into catalogue. Price = "+price);
+            }
+        });
+    }
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                try {
+                    int price = (int)catalogue.get(title);
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price));
+                } catch (NullPointerException e) {
+                    reply.setContent("not-available");
+                }
+
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                try {
+                    catalogue.remove(title);
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title+" sold to agent "+msg.getSender().getName());
+                } catch (NullPointerException e) {
+                    reply.setContent("not-available");
+                }
+
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+}
