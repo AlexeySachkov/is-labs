5e0e2f2450c03e03f607c4786fab48cf158a4ec2
Елизаров Евгений <elizarov.e.99@gmail.com>
2020-12-01 11:27:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import jade.Boot;
 
 public class Main {
     public static void main(String[] args) {
-
+        /* Модель с покупателем и двумя продавцами, первый - онлайн магазин по типу Амазон, второй - обычный книжный магазин*/
         String[] services = {"-giu", "-agents", "agentSellerFromOnline:SellerFromOnline"};
         Boot.main(services);
         String[] services2 = {"-giu", "-agents", "agentSellerFromShop:SellerFromShop"};
