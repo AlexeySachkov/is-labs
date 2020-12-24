7604872859dbceee17a12defdf6456fa1b894136
Alex Dubrovin <48681897+alex327512@users.noreply.github.com>
2020-12-24 03:07:29 +0300

    Dubrovin. M-20-IST-5. Lab2

---
+++ terminal.py
@@ -0,0 +1,226 @@
+# Покупатель приходит на вокзал и хочет купить билет.
+# Есть три возможных города, в которые он может поехать.
+# А именно: Нижний Новгород, Москва и Санкт-Петербург.
+# Продавец предлагает возможные варианты.
+# А дальше выбор за покупателем.
+
+# Первый клиент
+# [saler] 24/12/2020 02:52:47.409 --> Добрый день.
+# [saler] 24/12/2020 02:52:47.409 --> В какой город вы хотели бы поехать?
+# [customer] 24/12/2020 02:52:47.417 --> Здравствуйте.
+# [customer] 24/12/2020 02:52:47.417 --> Я хотел бы поехать в город Питер.
+# [saler] 24/12/2020 02:52:47.424 --> Доступен день: Понедельник.
+# [saler] 24/12/2020 02:52:47.426 --> Доступен день: Четверг.
+# [saler] 24/12/2020 02:52:47.428 --> Доступен день: Воскресенье.
+# [customer] 24/12/2020 02:52:47.432 --> Мне подходит Воскресенье.
+# [saler] 24/12/2020 02:52:47.436 --> Доступно время день: 3:00.
+# [saler] 24/12/2020 02:52:47.436 --> Доступно время день: 15:00.
+# [saler] 24/12/2020 02:52:47.437 --> Доступно время день: 23:00.
+# [customer] 24/12/2020 02:52:47.441 --> Мне не подходит это время. Всего доброго.
+# [saler] 24/12/2020 02:52:47.444 --> Нам жаль, что мы не смогли вам помочь.
+# [saler] 24/12/2020 02:52:47.444 --> Будем рады увидеть вас снова.
+
+# Второй клиент
+# [saler] 24/12/2020 02:54:41.311 --> Добрый день.
+# [saler] 24/12/2020 02:54:41.312 --> В какой город вы хотели бы поехать?
+# [customer] 24/12/2020 02:54:41.319 --> Здравствуйте.
+# [customer] 24/12/2020 02:54:41.321 --> Я хотел бы поехать в город Москва.
+# [saler] 24/12/2020 02:54:41.329 --> Доступен день: Вторник.
+# [saler] 24/12/2020 02:54:41.330 --> Доступен день: Четверг.
+# [saler] 24/12/2020 02:54:41.330 --> Доступен день: Воскресенье.
+# [customer] 24/12/2020 02:54:41.339 --> Мне не подходят указанные дни. Всего доброго.
+# [saler] 24/12/2020 02:54:41.343 --> Нам жаль, что мы не смогли вам помочь.
+# [saler] 24/12/2020 02:54:41.344 --> Будем рады увидеть вас снова.
+
+# Третий клиент
+# [saler] 24/12/2020 02:58:19.486 --> Добрый день.
+# [saler] 24/12/2020 02:58:19.487 --> В какой город вы хотели бы поехать?
+# [customer] 24/12/2020 02:58:19.494 --> Здравствуйте.
+# [customer] 24/12/2020 02:58:19.495 --> Я хотел бы поехать в город НН.
+# [saler] 24/12/2020 02:58:19.503 --> Доступен день: Вторник.
+# [saler] 24/12/2020 02:58:19.504 --> Доступен день: Среда.
+# [saler] 24/12/2020 02:58:19.505 --> Доступен день: Воскресенье.
+# [customer] 24/12/2020 02:58:19.513 --> Мне подходит Воскресенье.
+# [saler] 24/12/2020 02:58:19.521 --> Доступно время день: 2:00.
+# [saler] 24/12/2020 02:58:19.521 --> Доступно время день: 11:00.
+# [saler] 24/12/2020 02:58:19.523 --> Доступно время день: 20:00.
+# [customer] 24/12/2020 02:58:19.531 --> Мне не подходит это время. Всего доброго.
+# [saler] 24/12/2020 02:58:19.535 --> Нам жаль, что мы не смогли вам помочь.
+# [saler] 24/12/2020 02:58:19.535 --> Будем рады увидеть вас снова.
+
+# Четвертый клиент
+# [saler] 24/12/2020 02:59:16.953 --> Добрый день.
+# [saler] 24/12/2020 02:59:16.953 --> В какой город вы хотели бы поехать?
+# [customer] 24/12/2020 02:59:16.958 --> Здравствуйте.
+# [customer] 24/12/2020 02:59:16.958 --> Я хотел бы поехать в город НН.
+# [saler] 24/12/2020 02:59:16.962 --> Доступен день: Понедельник.
+# [saler] 24/12/2020 02:59:16.963 --> Доступен день: Четверг.
+# [saler] 24/12/2020 02:59:16.963 --> Доступен день: Пятница.
+# [customer] 24/12/2020 02:59:16.967 --> Мне подходит Четверг.
+# [saler] 24/12/2020 02:59:16.971 --> Доступно время день: 3:00.
+# [saler] 24/12/2020 02:59:16.972 --> Доступно время день: 13:00.
+# [saler] 24/12/2020 02:59:16.972 --> Доступно время день: 18:00.
+# [customer] 24/12/2020 02:59:16.977 --> Мне подходит 13:00.
+# [saler] 24/12/2020 02:59:16.980 --> Отлично! Рады были вам помочь.
+# [saler] 24/12/2020 02:59:16.981 --> Будем ждать вас снова.
+
+# Пятый клиент
+# [saler] 24/12/2020 03:02:22.808 --> Добрый день.
+# [saler] 24/12/2020 03:02:22.808 --> В какой город вы хотели бы поехать?
+# [customer] 24/12/2020 03:02:22.816 --> Здравствуйте.
+# [customer] 24/12/2020 03:02:22.817 --> Я хотел бы поехать в город Питер.
+# [saler] 24/12/2020 03:02:22.825 --> Доступен день: Понедельник.
+# [saler] 24/12/2020 03:02:22.826 --> Доступен день: Четверг.
+# [saler] 24/12/2020 03:02:22.827 --> Доступен день: Пятница.
+# [customer] 24/12/2020 03:02:22.833 --> Мне подходит Четверг.
+# [saler] 24/12/2020 03:02:22.837 --> Доступно время день: 6:00.
+# [saler] 24/12/2020 03:02:22.837 --> Доступно время день: 12:00.
+# [saler] 24/12/2020 03:02:22.839 --> Доступно время день: 22:00.
+# [customer] 24/12/2020 03:02:22.844 --> Мне подходит 22:00.
+# [saler] 24/12/2020 03:02:22.848 --> Отлично! Рады были вам помочь.
+# [saler] 24/12/2020 03:02:22.848 --> Будем ждать вас снова.
+
+import json
+import random
+from pade.acl.messages import ACLMessage
+from pade.misc.utility import display_message, start_loop
+from pade.core.agent import Agent
+from pade.acl.aid import AID
+
+class Saler(Agent):
+    def __init__(self, aid):
+        super(Saler, self).__init__(aid=aid, debug=False)
+        self.days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+
+    def on_start(self):
+        super().on_start()
+        self.call_later(10, self.send_proposal)
+
+    def send_proposal(self):
+        display_message(self.aid.localname, "Добрый день.")
+        display_message(self.aid.localname, "В какой город вы хотели бы поехать?")
+        message = ACLMessage()
+        message.set_performative(ACLMessage.PROPOSE)
+        message.add_receiver(AID(name="customer@localhost:7000"))
+        self.send(message)
+    
+    def react(self, message):
+        super(Saler, self).react(message)
+        
+        if message.performative == ACLMessage.PROPOSE:
+            content = json.loads(message.content)
+            choose = content['city']
+            if choose == 'Москва':
+                self.day_one = self.days[random.randint(0, 1)]
+                self.day_two = self.days[random.randint(2, 3)]
+                self.day_three = self.days[random.randint(4, 6)]
+            elif choose == 'Питер':
+                self.day_one = self.days[random.randint(0, 1)]
+                self.day_two = self.days[random.randint(2, 3)]
+                self.day_three = self.days[random.randint(4, 6)]
+            else:
+                self.day_one = self.days[random.randint(0, 1)]
+                self.day_two = self.days[random.randint(2, 3)]
+                self.day_three = self.days[random.randint(4, 6)]
+            
+            message = ACLMessage()
+            display_message(self.aid.localname, "Доступен день: {}.".format(self.day_one))
+            display_message(self.aid.localname, "Доступен день: {}.".format(self.day_two))
+            display_message(self.aid.localname, "Доступен день: {}.".format(self.day_three))
+
+            message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
+            message.set_content(json.dumps({'day_one': self.day_one, 'day_two': self.day_two, 'day_three': self.day_three, 'selected_day': False}))
+            message.add_receiver(AID(name="customer@localhost:7000"))
+            self.send(message)
+
+        elif message.performative == ACLMessage.ACCEPT_PROPOSAL:
+            content = json.loads(message.content)
+            time = content['time']
+            if not time:
+                self.time_one = random.randint(0, 8)
+                self.time_two = random.randint(9, 16)
+                self.time_three = random.randint(16, 24)
+                message = ACLMessage()
+                display_message(self.aid.localname, "Доступно время день: {}:00.".format(self.time_one))
+                display_message(self.aid.localname, "Доступно время день: {}:00.".format(self.time_two))
+                display_message(self.aid.localname, "Доступно время день: {}:00.".format(self.time_three))
+                message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
+                message.set_content(json.dumps({'time_one': self.time_one, 'time_two': self.time_two, 'time_three': self.time_three, 'selected_day': True}))
+                message.add_receiver(AID(name="customer@localhost:7000"))
+                self.send(message)
+
+            else:
+                display_message(self.aid.localname, "Отлично! Рады были вам помочь.")
+                display_message(self.aid.localname, "Будем ждать вас снова.")
+                message = ACLMessage()
+                message.set_performative(ACLMessage.REJECT_PROPOSAL)
+                message.add_receiver(AID(name="customer@localhost:7000"))
+                self.send(message)
+        elif message.performative == ACLMessage.REJECT_PROPOSAL:
+            display_message(self.aid.localname, "Нам жаль, что мы не смогли вам помочь.")
+            display_message(self.aid.localname, "Будем рады увидеть вас снова.")
+            message = ACLMessage()
+            message.set_performative(ACLMessage.REJECT_PROPOSAL)
+            message.add_receiver(AID(name="customer@localhost:7000"))
+            self.send(message)
+
+
+class Customer(Agent):
+    def __init__(self, aid):
+        super(Customer, self).__init__(aid=aid, debug=False)
+        self.cities = ['Москва', 'Питер', 'НН']
+        self.days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+
+
+    def react(self, message):
+        super(Customer, self).react(message)
+        if message.performative == ACLMessage.PROPOSE:
+            self.rand_city = self.cities[random.randint(0,2)]
+            message = ACLMessage()
+            display_message(self.aid.localname, "Здравствуйте.")
+            display_message(self.aid.localname, "Я хотел бы поехать в город {}.".format(self.rand_city))
+            message.set_performative(ACLMessage.PROPOSE)
+            message.set_content(json.dumps({'city': self.rand_city}))
+            message.add_receiver(AID(name="saler@localhost:7500"))
+            self.send(message)
+        elif message.performative == ACLMessage.ACCEPT_PROPOSAL:
+            content = json.loads(message.content)
+            self.selected_day = content['selected_day']
+            if not self.selected_day:
+                self.suitable = random.randint(0,1)
+                message = ACLMessage()
+                if self.suitable == 1:
+                    self.able_days = [content['day_one'], content['day_two'], content['day_three']]
+                    self.rand_suit_day = self.able_days[random.randint(0,2)]
+                    display_message(self.aid.localname, "Мне подходит {}.".format(self.rand_suit_day))
+                    message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
+                    message.set_content(json.dumps({'suit_day': self.rand_suit_day, 'time': False}))
+                else:
+                    display_message(self.aid.localname, "Мне не подходят указанные дни. Всего доброго.")
+                    message.set_performative(ACLMessage.REJECT_PROPOSAL)
+                message.add_receiver(AID(name="saler@localhost:7500"))
+                self.send(message)
+            else: 
+                self.suitable = random.randint(0,1)
+                message = ACLMessage()
+                if self.suitable == 1:
+                    self.able_time = [content['time_one'], content['time_two'], content['time_three']]
+                    self.rand_suit_time = self.able_time[random.randint(0,2)]
+                    display_message(self.aid.localname, "Мне подходит {}:00.".format(self.rand_suit_time))
+                    message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
+                    message.set_content(json.dumps({'time': True}))
+                else:
+                    display_message(self.aid.localname, "Мне не подходит это время. Всего доброго.")
+                    message.set_performative(ACLMessage.REJECT_PROPOSAL)
+                message.add_receiver(AID(name="saler@localhost:7500"))
+                self.send(message)
+
+    
+if __name__ == '__main__':
+    agents = list()
+    customer = Customer(AID(name='customer@localhost:7000'))
+    saler = Saler(AID(name="saler@localhost:7500"))
+    agents.append(customer)
+    agents.append(saler)
+    start_loop(agents)
+
