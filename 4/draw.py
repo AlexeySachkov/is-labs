4313ab2ea8b1f5e0b0e8644c9320c97e553a58e0
Aleksandr Chutkov <assus0@yandex.ru>
2020-12-24 11:49:33 +0300

    lab4

--- 4/draw.py
+++ 4/draw.py
@@ -5,8 +5,6 @@ Simple implementation of http://arxiv.org/pdf/1502.04623v2.pdf in TensorFlow
 
 Example Usage: 
 	python draw.py --data_dir=/tmp/draw --read_attn=True --write_attn=True
-
-Author: Eric Jang
 """
 
 import tensorflow as tf
