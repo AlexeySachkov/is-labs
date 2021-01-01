3b5b5d30a4a2db94b4b11f33cd59a23a311cbd2d
ksenia03581 <59887394+ksenia03581@users.noreply.github.com>
2021-01-01 14:32:01 +0300

    Update lab2.py

--- шишкина ксения/lab2.py
+++ шишкина ксения/lab2.py
@@ -23,13 +23,12 @@ class AgeControl(Agent):
             
             content = json.loads(message.content)
             ageRandomCustomer = int(content['ageRandomCustomer'])
-            display_message(self.aid.localname, "Мне : {}".format(ageRandomCustomer))
-            message = ACLMessage()
             
+            message = ACLMessage()
+            self.timeNow+=1
+            print(self.timeNow)
+            display_message(self.aid.localname, "Мне : {}".format(ageRandomCustomer))
             if ageRandomCustomer <=18:
-                
-                self.timeNow+=1
-                print(self.timeNow)
                 if self.timeNow == 24:
                     message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))               
@@ -40,14 +39,13 @@ class AgeControl(Agent):
                     display_message(self.aid.localname, "Отправить родителям")
                     self.AgeControlPos =1
                     message.set_content(json.dumps({'AgeControlPos':1}))
-                elif self.timeNow >=6 & self.timeNow <=22 :
+                elif (self.timeNow >=6) and (self.timeNow <21) :
                     message.set_performative(ACLMessage.REJECT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))
                     display_message(self.aid.localname, "Проходите")
                     self.AgeControlPos =0
                     message.set_content(json.dumps({'AgeControlPos':0}))
-
-                elif self.timeNow < 22 & self.timeNow<24 :
+                elif (self.timeNow >= 21) and (self.timeNow<24) :
                     message.set_performative(ACLMessage.REJECT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))
                     display_message(self.aid.localname, "Отправить родителям")
@@ -57,9 +55,6 @@ class AgeControl(Agent):
                 
 
             elif ageRandomCustomer>18:
-                
-                self.timeNow+=1
-                print(self.timeNow)
                 if self.timeNow == 24:
                     message.set_performative(ACLMessage.ACCEPT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))               
@@ -70,14 +65,13 @@ class AgeControl(Agent):
                     display_message(self.aid.localname, "Проходите")
                     self.AgeControlPos =0
                     message.set_content(json.dumps({'AgeControlPos':self.AgeControlPos}))
-                elif self.timeNow >=6 & self.timeNow <=22 :
+                elif (self.timeNow >=6) and (self.timeNow <21) :
                     message.set_performative(ACLMessage.REJECT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))
-                    display_message(self.aid.localname, "Проходите")
-                    self.AgeControlPos =0
-                    message.set_content(json.dumps({'AgeControlPos':0}))
-
-                elif self.timeNow < 22 & self.timeNow <24 :
+                    display_message(self.aid.localname, "Проход закрыт")
+                    self.AgeControlPos =1
+                    message.set_content(json.dumps({'AgeControlPos':self.AgeControlPos}))
+                elif (self.timeNow >= 21) and (self.timeNow <24) :
                     message.set_performative(ACLMessage.REJECT_PROPOSAL)
                     message.add_receiver(AID(name="RandomCustomer@localhost:8011"))
                     display_message(self.aid.localname, "Проходите")
@@ -89,7 +83,7 @@ class AgeControl(Agent):
 class RandomCustomer(Agent):
     def __init__(self, aid):
         super(RandomCustomer, self).__init__(aid=aid, debug=False)
-        self.ageRandomCustomer = 0
+        self.ageRandomCustomer = random.randint(5,65)
 
     def on_start(self):
         super().on_start()
@@ -134,120 +128,122 @@ if __name__ == '__main__':
 
 
 
+
 # 1
-# [AgeControl] 09/12/2020 14:34:31.166 --> Отправить родителям
-# [RandomCustomer] 09/12/2020 14:34:31.171 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.173 --> Прийду позже
-# [AgeControl] 09/12/2020 14:34:31.176 --> Мне : 12
+# [AgeControl] 31/12/2020 01:40:49.804 --> Мне : 62
+# [AgeControl] 31/12/2020 01:40:49.805 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:49.808 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.810 --> Захожу
 # 2
-# [AgeControl] 09/12/2020 14:34:31.183 --> Отправить родителям
-# [RandomCustomer] 09/12/2020 14:34:31.187 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.188 --> Прийду позже
-# [AgeControl] 09/12/2020 14:34:31.191 --> Мне : 34
+# [AgeControl] 31/12/2020 01:40:49.820 --> Мне : 45
+# [AgeControl] 31/12/2020 01:40:49.821 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:49.836 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.837 --> Захожу
 # 3
-# [AgeControl] 09/12/2020 14:34:31.193 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.196 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.199 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.203 --> Мне : 34
+# [AgeControl] 31/12/2020 01:40:49.842 --> Мне : 24
+# [AgeControl] 31/12/2020 01:40:49.842 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:49.846 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.851 --> Захожу
 # 4
-# [AgeControl] 09/12/2020 14:34:31.204 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.207 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.208 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.210 --> Мне : 25
+# [AgeControl] 31/12/2020 01:40:49.856 --> Мне : 61
+# [AgeControl] 31/12/2020 01:40:49.857 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:49.860 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.862 --> Захожу
 # 5
-# [AgeControl] 09/12/2020 14:34:31.216 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.221 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.222 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.225 --> Мне : 60
+# [AgeControl] 31/12/2020 01:40:49.870 --> Мне : 7
+# [AgeControl] 31/12/2020 01:40:49.871 --> Отправить родителям
+# [RandomCustomer] 31/12/2020 01:40:49.874 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.875 --> Прийду позже
 # 6
-# [AgeControl] 09/12/2020 14:34:31.227 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.230 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.233 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.237 --> Мне : 32
+# [AgeControl] 31/12/2020 01:40:49.878 --> Мне : 64
+# [AgeControl] 31/12/2020 01:40:49.878 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.888 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.889 --> Прийду позже
 # 7
-# [AgeControl] 09/12/2020 14:34:31.238 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.241 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.242 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.245 --> Мне : 37
+# [AgeControl] 31/12/2020 01:40:49.892 --> Мне : 28
+# [AgeControl] 31/12/2020 01:40:49.893 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.896 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.902 --> Прийду позже
 # 8
-# [AgeControl] 09/12/2020 14:34:31.251 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.255 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.256 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.259 --> Мне : 10
+# [AgeControl] 31/12/2020 01:40:49.918 --> Мне : 44
+# [AgeControl] 31/12/2020 01:40:49.920 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.924 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.925 --> Прийду позже
 # 9
-# [AgeControl] 09/12/2020 14:34:31.260 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.269 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.270 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.273 --> Мне : 32
+# [AgeControl] 31/12/2020 01:40:49.928 --> Мне : 49
+# [AgeControl] 31/12/2020 01:40:49.929 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.938 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.939 --> Прийду позже
 # 10
-# [AgeControl] 09/12/2020 14:34:31.274 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.277 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.282 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.296 --> Мне : 30
+# [AgeControl] 31/12/2020 01:40:49.943 --> Мне : 21
+# [AgeControl] 31/12/2020 01:40:49.944 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.948 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.952 --> Прийду позже
 # 11
-# [AgeControl] 09/12/2020 14:34:31.299 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.304 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.305 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.308 --> Мне : 21
+# [AgeControl] 31/12/2020 01:40:49.956 --> Мне : 63
+# [AgeControl] 31/12/2020 01:40:49.957 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.960 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.960 --> Прийду позже
 # 12
-# [AgeControl] 09/12/2020 14:34:31.310 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.313 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.316 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.320 --> Мне : 65
+# [AgeControl] 31/12/2020 01:40:49.968 --> Мне : 25
+# [AgeControl] 31/12/2020 01:40:49.970 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.973 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.974 --> Прийду позже
 # 13
-# [AgeControl] 09/12/2020 14:34:31.321 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.325 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.325 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.328 --> Мне : 29
+# [AgeControl] 31/12/2020 01:40:49.977 --> Мне : 41
+# [AgeControl] 31/12/2020 01:40:49.978 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:49.981 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:49.986 --> Прийду позже
 # 14
-# [AgeControl] 09/12/2020 14:34:31.333 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.337 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.338 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.341 --> Мне : 7
+# [AgeControl] 31/12/2020 01:40:50.002 --> Мне : 32
+# [AgeControl] 31/12/2020 01:40:50.003 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.007 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.008 --> Прийду позже
 # 15
-# [AgeControl] 09/12/2020 14:34:31.343 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.346 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.351 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.354 --> Мне : 16
+# [AgeControl] 31/12/2020 01:40:50.011 --> Мне : 25
+# [AgeControl] 31/12/2020 01:40:50.011 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.015 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.019 --> Прийду позже
 # 16
-# [AgeControl] 09/12/2020 14:34:31.355 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.358 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.359 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.363 --> Мне : 44
+# [AgeControl] 31/12/2020 01:40:50.023 --> Мне : 60
+# [AgeControl] 31/12/2020 01:40:50.024 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.027 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.028 --> Прийду позже
 # 17
-# [AgeControl] 09/12/2020 14:34:31.367 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.370 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.371 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.374 --> Мне : 21
+# [AgeControl] 31/12/2020 01:40:50.034 --> Мне : 28
+# [AgeControl] 31/12/2020 01:40:50.036 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.039 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.040 --> Прийду позже
 # 18
-# [AgeControl] 09/12/2020 14:34:31.375 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.379 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.383 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.387 --> Мне : 65
+# [AgeControl] 31/12/2020 01:40:50.043 --> Мне : 22
+# [AgeControl] 31/12/2020 01:40:50.044 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.047 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.052 --> Прийду позже
 # 19
-# [AgeControl] 09/12/2020 14:34:31.388 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.391 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.392 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.395 --> Мне : 27
+# [AgeControl] 31/12/2020 01:40:50.055 --> Мне : 11
+# [AgeControl] 31/12/2020 01:40:50.056 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:50.060 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.060 --> Захожу
 # 20
-# [AgeControl] 09/12/2020 14:34:31.400 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.404 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.405 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.408 --> Мне : 23
+# [AgeControl] 31/12/2020 01:40:50.068 --> Мне : 38
+# [AgeControl] 31/12/2020 01:40:50.069 --> Проход закрыт
+# [RandomCustomer] 31/12/2020 01:40:50.072 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.073 --> Прийду позже
 # 21
-# [AgeControl] 09/12/2020 14:34:31.409 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.412 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.416 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.419 --> Мне : 17
+# [AgeControl] 31/12/2020 01:40:50.076 --> Мне : 38
+# [AgeControl] 31/12/2020 01:40:50.077 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:50.080 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.085 --> Захожу
 # 22
-# [AgeControl] 09/12/2020 14:34:31.421 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.425 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.426 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.430 --> Мне : 14
+# [AgeControl] 31/12/2020 01:40:50.088 --> Мне : 12
+# [AgeControl] 31/12/2020 01:40:50.089 --> Отправить родителям
+# [RandomCustomer] 31/12/2020 01:40:50.092 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.093 --> Прийду позже
 # 23
-# [AgeControl] 09/12/2020 14:34:31.434 --> Проходите
-# [RandomCustomer] 09/12/2020 14:34:31.437 --> Sending Value
-# [RandomCustomer] 09/12/2020 14:34:31.438 --> Захожу
-# [AgeControl] 09/12/2020 14:34:31.442 --> Мне : 47
+# [AgeControl] 31/12/2020 01:40:50.101 --> Мне : 55
+# [AgeControl] 31/12/2020 01:40:50.102 --> Проходите
+# [RandomCustomer] 31/12/2020 01:40:50.105 --> Sending Value
+# [RandomCustomer] 31/12/2020 01:40:50.106 --> Захожу
 # 24
-# [AgeControl] 09/12/2020 14:34:31.443 --> Смена закончена
+# [AgeControl] 31/12/2020 01:40:50.120 --> Мне : 52
+# [AgeControl] 31/12/2020 01:40:50.122 --> Смена закончена
