0dd1dad0b399bd0aa3d73995c8b40762c2a4a28b
eugenkur <67636067+eugenkur@users.noreply.github.com>
2021-01-13 19:57:31 +0300

    Create item.java

---
+++ item.java
@@ -0,0 +1,14 @@
+package laba2_ceramicproductstore;
+
+/**
+ *
+ * @author Евгений
+ */
+public class item {
+    public String name;
+    public int price;
+        public item (String name, int price){
+            this.name = name;
+            this.price = price;
+        }
+}
