341b493ed5e353fd9e595e8dc98f83f756972947
Anastasia <stacy.forever@mail.ru>
2021-01-16 21:48:32 +0300

    Add lab 2

---
+++ Stulova M20-IST-1/2/src/qwe/OrderAgent.java
@@ -0,0 +1,42 @@
+package qwe;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class OrderAgent extends Agent {
+    protected void setup() {
+        addBehaviour(new SimpleBehaviour(this) {
+          private boolean finished = false;
+          public String DestinationPoint = "Nizhny Novgorod";
+
+            AID[] resources = {
+                    new AID("Resource1@192.168.1.93:1099/JADE", AID.ISGUID),
+                    new AID("Resource2@192.168.1.93:1099/JADE", AID.ISGUID),
+                    new AID("Resource3@192.168.1.93:1099/JADE", AID.ISGUID),
+                    new AID("Resource4@192.168.1.93:1099/JADE", AID.ISGUID)};
+
+            @Override
+            public void action() {
+                System.out.println(getLocalName() + " is active");
+                ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+                message.setOntology("TestOntology");
+                message.setContent(DestinationPoint);
+
+                for (AID resource : resources) {
+                    message.addReceiver(resource);
+                }
+                send(message);
+                finished = true;
+            }
+
+            @Override
+            public boolean done()
+            {
+                return finished;
+            }
+        });
+    }
+}
+
